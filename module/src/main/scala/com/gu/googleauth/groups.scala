4e04076fafd89311039fe04689a6cf892bfe4f2c
Jenny Sivapalan <jennifer.sivapalan@guardian.co.uk>
2014-12-01 16:43:07 +0000

    Check if a user is in a google group

---
+++ module/src/main/scala/com/gu/googleauth/groups.scala
@@ -0,0 +1,25 @@
+package com.gu.googleauth
+
+import com.google.gdata.client.appsforyourdomain.AppsGroupsService
+import scala.collection.JavaConverters._
+
+
+case class GoogleGroupConfig(
+  adminUser: String,
+  adminPassword: String,
+  domain: String,
+  applicationName: String,
+  directOnly: Boolean = false)
+
+object GoogleGroupChecker {
+
+  def userIsInGroup(config: GoogleGroupConfig, userEmail: String, groupEmail: String): Boolean = {
+    val service = new AppsGroupsService(config.adminUser, config.adminPassword, config.domain, config.applicationName)
+    getGroupIds(service, config.directOnly, userEmail).contains(groupEmail)
+  }
+
+  private def getGroupIds(service: AppsGroupsService, directOnly: Boolean, userEmail: String): Set[String] =
+    service.retrieveGroups(userEmail, directOnly).getEntries.asScala.flatMap { entry =>
+      Option(entry.getProperty("groupId"))
+    }.toSet
+}
