779f4278021cde1930e88823ccfcffc087c13661
Adam Fisher <adam.fisher@guardian.co.uk>
2016-05-26 17:21:45 +0100

    Update example app to make it more realistic
    The example app is now much closer to what a real application using
    Play-Googleauth would look like. This should make it much easier to
    work out how to use the library.

--- example/app/controllers/Application.scala
+++ example/app/controllers/Application.scala
@@ -1,19 +1,26 @@
 package controllers
 
+import javax.inject.Inject
+
+import auth.AuthActions
 import com.gu.googleauth
+import play.api.Configuration
+import play.api.libs.ws.WSClient
 import play.api.mvc.Security.AuthenticatedRequest
 import play.api.mvc.{Action, Controller}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-class Application extends Controller with AuthActions {
+
+class Application @Inject() (override val wsClient: WSClient, override val conf: Configuration) extends AuthActions with Controller {
   type GoogleAuthRequest[A] = AuthenticatedRequest[A, googleauth.UserIdentity]
 
   def index = Action { request => Ok(views.html.index(request)) }
 
   def authenticated = AuthAction { request => Ok(views.html.authenticated(request)) }
 
-  def authenticatedAndInGroup = (AuthAction andThen requireGroup[GoogleAuthRequest](includedGroups = Set("membership.dev@guardian.co.uk"))) {
+  // checks Google group membership on every request
+  def authenticatedAndInGroup = (AuthAction andThen requireGroup[GoogleAuthRequest](includedGroups = requiredGoogleGroups)) {
     request => Ok(views.html.authenticated(request))
   }
 }
--- example/app/controllers/Login.scala
+++ example/app/controllers/Login.scala
@@ -1,67 +1,38 @@
 package controllers
 
 import javax.inject.Inject
-import play.api.mvc._
-import play.api.libs.concurrent.Execution.Implicits._
-import play.api.libs.json.Json
+
+import auth.AuthActions
+import play.api.Configuration
 import play.api.libs.ws.WSClient
-import scala.concurrent.Future
-import com.gu.googleauth._
-import org.joda.time.Duration
+import play.api.mvc._
 
-trait AuthActions extends Actions with Filters {
-  val loginTarget: Call = routes.Login.loginAction()
-  val authConfig = ExampleConfiguration.googleAuthConfig
-  lazy val groupChecker = new GoogleGroupChecker(???) // Can't share these credentials!
-}
 
-object ExampleConfiguration {
-  val googleAuthConfig =
-    GoogleAuthConfig(
-      "863070799113-9oefm3nnjf0hu4g9k3k1ue3fopfvrtpg.apps.googleusercontent.com",  // The client ID from the dev console
-      "5uLmlI8afy5vufKFWXWS2GPw",                  // The client secret from the dev console
-      "http://localhost:9000/oauth2callback",      // The redirect URL Google send users back to (must be the same as
-                                                   //    that configured in the developer console)
-      Some("guardian.co.uk"),                      // Google App domain to restrict login
-      Some(Duration.standardHours(1)),             // Force the user to re-enter their credentials if they haven't done
-                                                   //    so in the last hour (this is stupid unless you are testing :)
-      true                                         // Re-authenticate (without prompting) with google when session expires
-    )
-
-}
-
-class Login @Inject() (implicit override val wsClient: WSClient) extends Controller with AuthActions {
-
-  import ExampleConfiguration.googleAuthConfig
-
-  val ANTI_FORGERY_KEY = "antiForgeryToken"
-  val requiredGoogleGroups = Set("example")
-
-  override val defaultRedirectTarget = routes.Login.login()
-  override val failureRedirectTarget: Call = routes.Login.login()
-
+class Login @Inject() (override val wsClient: WSClient, override val conf: Configuration) extends AuthActions with Controller {
+  /**
+   * Shows UI for login button and logout error feedback
+   */
   def login = Action { request =>
     val error = request.flash.get("error")
     Ok(views.html.login(error))
   }
 
   /*
-  Redirect to Google with anti forgery token (that we keep in session storage - note that flashing is NOT secure)
+   * Redirect to Google with anti forgery token (that we keep in session storage - note that flashing is NOT secure).
    */
   def loginAction = Action.async { implicit request =>
     startGoogleLogin()
   }
 
   /*
-  Looks up user's identity via Google and enforces required Google groups
+   * Looks up user's identity via Google and (optionally) enforces required Google groups at login time.
+   *
+   * To re-check Google group membership on every page request you can use the `requireGroup` filter
+   * (see `Application.scala`).
    */
   def oauth2Callback = Action.async { implicit request =>
-    (for {
-      identity <- checkIdentity()
-      _ <- enforceGoogleGroups(identity, requiredGoogleGroups, groupChecker)  // if Google group membership is required
-    } yield {
-      setupSessionWhenSuccessful(identity)
-    }).merge
+    // processOauth2Callback()  // without Google group membership checks
+    processOauth2Callback(requiredGoogleGroups, groupChecker)  // with optional Google group checks
   }
 
   def logout = Action { implicit request =>
